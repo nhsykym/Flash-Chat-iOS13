84e9918fc2e5cb0fcd13966edbcc595a476f10ea
Naohisa Yokoyama <nhsykym@gmail.com>
2020-06-07 23:30:03 +0900

    Add features(Add/Fetch messages)

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by 横山尚久 on 2020/06/06.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
